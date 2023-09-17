59c1678203ba7dd2fa4aa2c154d597812f1d2d16
baansalpranav62 <bansalpranav62@gmail.com>
2023-09-17 19:20:15 +0530

    created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+import Wave from "../../img/wave.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Gitub from "@iconscout/react-unicons/icons/uil-github";
+import Dog from "../../img/Dog.png";
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+
+      <img src={Wave} alt="" style={{ width: "100%",height:"35vh"}} />
+      
+      <div className="f-content">
+        <span style={{marginTop:'100px'}}>bansalpranav62@gmail.com</span>
+        <span style={{marginTop:'100px'}}>+91-7827848707</span>
+      <img src={Dog} alt="" style={{height:"35vh"}}></img>
+        <div className="f-icons" style={{marginTop:'150px'}}>
+          {/* <Insta color="white" size={"3rem"} onClick={()=> window.open("https://www.instagram.com/naman_malhan/", "_blank")}/>
+          <Facebook color="white" size={"3rem"} onClick={()=> window.open("https://www.facebook.com/Naman.malhan.1871998/", "_blank")}/> */}
+          <Gitub color="white" size={"3rem"} onClick={()=> window.open("https://github.com/baansalpranav62", "_blank")} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
